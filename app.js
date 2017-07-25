33e104ccd51258901f3a41e0197c05cf8f3ad2c1
silviojof <silviojof@gmail.com>
2017-07-25 02:34:19 -0300

    adding mongoose

--- app.js
+++ app.js
@@ -22,6 +22,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// API's
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/bookshop');
+var Books = require('./models/books.js');
+
+//---->> POST BOOKS <----//
+app.post('/books', function(req, res) {
+    var book = req.body;
+    Books.create(book, function(err, books) {
+      if(err) {
+        throw err;
+      }
+      res.json(books);
+    })
+});
+
+// END API's
+
+
 app.get('*', function(req, res){
  res.sendFile(path.resolve(__dirname,
 'public', 'index.html'))
